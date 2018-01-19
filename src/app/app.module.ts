9bbdcdc92c1065ddd4a8cc5a89a78586b02177f1
rjmcbain <rmcbain01@gmail.com>
2018-01-19 09:01:24 -0700

    fixed turrets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { InternalDefensesModule } from '/internal-defenses/internal-defenses.module';
-import { ExternalDefensesModule } from '/external-defenses/external-defenses.module';
+import { InternalDefensesModule } from './internal-defenses/internal-defenses.module';
+import { ExternalDefensesModule } from './external-defenses/external-defenses.module';
 
 
 
